ba768f4e86735ccec4f34c12c1f39b08c1206b0a
AntonelaNespoli <antonela.nespoli@gmail.com>
2017-06-11 12:34:17 -0300

    Punto 2, resuelto

--- script_trabajo_especial.m
+++ script_trabajo_especial.m
@@ -3,16 +3,19 @@
  clear %elimina las variables que se declaran en el  workspace
  clc %limpia la pantalla cada vez que corre
  
+%Declaro mi primer EPSILON y hago funcionar al motor por primera vez,
+%luego genero su correspondiente grafico:
 
- epsilon = 0.1;
+epsilon_a = 0.1;
  
 tic
 
 %Llamo a mi motor de Montecarlo
-[probabilidad, todas_las_probabilidades] = calcular_fallos_sucesivos(epsilon);
+[probabilidad, todas_las_probabilidades] = calcular_fallos_sucesivos(epsilon_a);
 
-% Mostro por pantalla la propabilidad que me dio mi motor
-fprintf('La probabilidad de exito es: %d\n', probabilidad);
+% Muestro por pantalla la propabilidad que me dio mi motor
+fprintf('La probabilidad de exito con un Epsilon de 0.1, es: %d\n', probabilidad );
+fprintf('\n');
 
 % Genero el grafico
 figure, plot(todas_las_probabilidades);
@@ -24,4 +27,68 @@ ylabel('Probabilidad');
 ylim([0 1]);
 legend('Evolucion de la probabilidad', 'Probabilidad analitica');
 
-toc
+tiempo = toc;
+%Guardo el tic/toc en una variable y lo imprimo por pantalla junto a un string
+fprintf('El tiempo que tomo calcular la probabilidad es: %d\n', tiempo);
+fprintf('\n----------------------------------------------------\n');
+%-------------------------------------------------------------------------------
+
+%Declaro mi segundo EPSILON y hago funcionar al motor por segunda vez,
+%luego genero su correspondiente grafico:
+
+epsilon_b = 0.01;
+ 
+tic
+
+%Llamo a mi motor de Montecarlo
+[probabilidad, todas_las_probabilidades] = calcular_fallos_sucesivos(epsilon_b);
+
+% Muestro por pantalla la propabilidad que me dio mi motor
+fprintf('La probabilidad de exito con un Epsilon de 0.01, es: %d\n', probabilidad );
+fprintf('\n');
+
+% Genero el grafico
+figure, plot(todas_las_probabilidades);
+hold on;
+grid on;
+plot(ones(size(todas_las_probabilidades)) * 0.5, '--');
+xlabel('Numero de iteracion');
+ylabel('Probabilidad');
+ylim([0 1]);
+legend('Evolucion de la probabilidad', 'Probabilidad analitica');
+
+tiempo = toc;
+%Guardo el tic/toc en una variable y lo imprimo por pantalla junto a un string
+fprintf('El tiempo que tomo calcular la probabilidad es: %d\n', tiempo);
+fprintf('\n----------------------------------------------------\n');
+%-------------------------------------------------------------------------------
+
+%Declaro mi tercer EPSILON y hago funcionar al motor por tercera vez,
+%luego genero su correspondiente grafico:
+
+epsilon_c = 0.001;
+ 
+tic
+
+%Llamo a mi motor de Montecarlo
+[probabilidad, todas_las_probabilidades] = calcular_fallos_sucesivos(epsilon_c);
+
+% Muestro por pantalla la propabilidad que me dio mi motor
+fprintf('La probabilidad de exito con un Epsilon de 0.001, es: %d\n', probabilidad );
+fprintf('\n');
+
+% Genero el grafico
+figure, plot(todas_las_probabilidades);
+hold on;
+grid on;
+plot(ones(size(todas_las_probabilidades)) * 0.5, '--');
+xlabel('Numero de iteracion');
+ylabel('Probabilidad');
+ylim([0 1]);
+legend('Evolucion de la probabilidad', 'Probabilidad analitica');
+
+tiempo = toc;
+%Guardo el tic/toc en una variable y lo imprimo por pantalla junto a un string
+fprintf('El tiempo que tomo calcular la probabilidad es: %d\n', tiempo);
+fprintf('\n----------------------------------------------------\n');
+%-------------------------------------------------------------------------------
